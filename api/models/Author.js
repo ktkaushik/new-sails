bc4cba8f440f36182663239a5b5401f5de7797a3
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2013-08-30 18:06:39 +0530

    added new models with associated schema for sails-mongoose

---
+++ api/models/Author.js
@@ -0,0 +1,16 @@
+/**
+ * Author
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ *
+ */
+
+module.exports = {
+  schema: true,
+  adapter: 'mongoose',
+  attributes: {
+    name: 'string',
+    books: [{type: 'Schema.Types.ObjectId', ref: 'Book'}]
+  }
+};
---
+++ api/models/Book.js
@@ -0,0 +1,22 @@
+/**
+ * Book
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ *
+ */
+
+module.exports = {
+
+  schema: true,
+  adapter: 'mongoose',
+
+  attributes: {
+    title: 'string',
+    _creator: {
+      // type: 'Schema.ObjectId'
+       //author: [{ type: 'Schema.Types.ObjectId', ref: 'Author' } ]
+      associate: { ref: 'Author' }
+    }
+  }
+};
